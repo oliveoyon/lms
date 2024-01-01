3e52f0b1b332ed67bd0a99235b04d43ac31cd5fa
oliveoyon <arifur123@gmail.com>
2024-01-01 18:42:18 +0600

    logo

--- resources/views/dashboard/admin/login1.blade.php
+++ resources/views/dashboard/admin/login1.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta http-equiv="x-ua-compatible" content="ie=edge" />
-  <title>BizTrack</title>
+  <title>EduFlow</title>
   <!-- MDB icon -->
   <link rel="icon" href="{{ asset('login/img/favicon.svg') }}" type="image/x-icon" />
   <!-- Font Awesome -->
@@ -26,14 +26,14 @@
             <div class="row g-0">
               <div class="col-md-6 col-lg-5 d-none d-md-block">
                 <img
-                  src="{{ asset('login/img/BizTrack.svg') }}"
+                  src="{{ asset('login/img/eduflow.jpg') }}"
                   alt="login form"
                   class="img-fluid" style="border-radius: 1rem 0 0 1rem;"
                 />
               </div>
               <div class="col-md-6 col-lg-7 d-flex align-items-center">
                 <div class="card-body p-4 p-lg-5 text-black">
-  
+
                     <form action="{{ route('admin.check') }}" method="post" autocomplete="off">
                         @if (Session::get('fail'))
                             <div class="alert alert-danger">
@@ -41,30 +41,30 @@
                             </div>
                         @endif
                         @csrf
-  
+
                     <div class="d-flex align-items-center mb-3 pb-1">
                       <i class="fas fa-cubes fa-2x me-3" style="color: #3f86b3;"></i>
-                      <span class="h1 fw-bold mb-0"><span style="color:#ff0000">Biz</span><span style="color:#548235">Track</span>
+                      <span class="h1 fw-bold mb-0"><span style="color:#ff0000">Edu</span><span style="color:#548235">Flow</span>
                     </div>
-  
+
                     <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-  
+
                     <div class="form-outline mb-4">
                       <input type="email" id="form2Example17" name="email" class="form-control form-control-lg @error('email') is-invalid @enderror" value="{{ old('email') }}" />
                       <label class="form-label" for="form2Example17">Email address</label>
                     </div>
-  
+
                     <div class="form-outline mb-4">
                       <input type="password" id="form2Example27" name="password" class="form-control form-control-lg @error('password') is-invalid @enderror" value="{{ old('email') }}"/>
                       <label class="form-label" for="form2Example27">Password</label>
                     </div>
-  
+
                     <div class="pt-1 mb-4">
                       <button class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
                     </div>
-  
+
                   </form>
-  
+
                 </div>
               </div>
             </div>
