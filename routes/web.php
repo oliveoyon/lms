586e61c432b385884717a105e2bfd24d8716b5f1
oliveoyon <arifur123@gmail.com>
2024-01-01 18:49:41 +0600

    logo

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.admin.login1');
     // return redirect('/admin/login');
 });
 
