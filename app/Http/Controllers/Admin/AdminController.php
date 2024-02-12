7f6e8bfdefd2a715d6b9abb3b0cbdf72c4ed2233
User <arifur123@gmail.com>
2024-02-12 12:35:41 +0600

    test

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
+use App\Models\Admin\Event;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -17,8 +19,24 @@ class AdminController extends Controller
                 (SELECT COUNT(*) FROM academic_students WHERE st_aca_status = 1) AS total_students,
                 (SELECT COUNT(*) FROM teachers WHERE teacher_status = 1) AS total_teachers,
                 (SELECT COUNT(*) FROM books WHERE book_status = 1) AS total_books,
-                (SELECT COUNT(*) FROM tr_assign_stds WHERE tr_assign_status = 1) AS total_assigned_students
-        ")[0];
+                (SELECT COUNT(*) FROM tr_assign_stds WHERE tr_assign_status = 1) AS total_assigned_students")[0];
+
+        $events = Event::where('event_status', 1)
+        ->whereDate('start_date', '>=', now())
+        ->select('event_title', 'start_date', 'end_date', 'url', 'color')
+        ->get()
+        ->map(function ($event) {
+            return [
+                'title' => $event->event_title,
+                'start' => Carbon::parse($event->start_date)->toDateTimeString(),
+                'end' => Carbon::parse($event->end_date)->toDateTimeString(),
+                'url' => $event->url,
+                'color' => $event->color,
+            ];
+        });
+
+        $send['eventsJson'] = $events->toJson();
+            
 
         return view('dashboard.admin.home', $send);
     }
